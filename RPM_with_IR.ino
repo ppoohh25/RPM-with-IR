c67c5f79a0ee742d12d5bf3cb91a47804bdeeed9
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2024-02-15 17:13:15 +0700

    Update RPM_with_IR.ino

--- RPM_with_IR.ino
+++ RPM_with_IR.ino
@@ -1,35 +1,50 @@
 #define IR_Sensor 2 /*D2 IR pin defined*/
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-int IR;
-float rev;
-float value=0;
-int rpm;
-int oldtime=0;
-int time;
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+volatile int tick = 0;
+volatile unsigned long time1 = 0;
+volatile unsigned long time2 = 0;
+volatile unsigned long rev = 0;
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
   pinMode(IR_Sensor, INPUT);   /*IR Pin D2 defined as Input*/
-  attachInterrupt(digitalPinToInterrupt(IR_Sensor), REV, FALLING);
+  attachInterrupt(digitalPinToInterrupt(IR_Sensor), REV, FALLING); // Change to RISING if IR sensor triggers on rising edge
+  Serial.begin(9600);
 }
-void loop(){
-  IR=digitalRead(IR_Sensor);  /*digital read function to check IR pin status*/
-  time=millis()-oldtime; //finds the time
-  rpm=(rev/time)*60000; //calculates rpm for blades
-  oldtime=millis(); //saves the current time
-  rev=0;
-  lcd.setCursor(0,0);
+
+void loop()
+{
+  unsigned long time_diff = time2 - time1;
+  float rpm = 0;
+  rpm = (1 / (time_diff/  60000.0));
+  //rev =0;
+  lcd.setCursor(0, 0);
   lcd.print("RPM is");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(rpm);
-  delay(1000);
+  Serial.println(rpm);
+  delay(200);
   lcd.clear();
 }
-void REV(){
-  rev++;
+
+
+void REV()
+{
+  if (tick == 0)
+  {
+    time1 = millis();
+    tick = 1;
+  }
+  else if (tick == 1)
+  {
+    time2 = millis();
+    tick = 0;
+    rev++;
+  }
 }
